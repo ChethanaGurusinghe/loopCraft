9022ab4bf46e1dd6d75bd65be1b8ace9758eef1c
ChethanaGurusinghe <https://github.com/ChethanaGurusinghe>
2026-02-13 22:15:34 +0530

    mysql part 3 done

--- data/posts.js
+++ data/posts.js
@@ -1,31 +1,60 @@
 const posts = {
   1: {
-  image: "https://res.cloudinary.com/dy07nxsrf/image/upload/v1770908799/Gemini_Generated_Image_19mh6919mh6919mh_bvkg7g.png",
-  file: "posts/mysql-part-1.html",   
+    image: "https://res.cloudinary.com/dy07nxsrf/image/upload/v1770908799/Gemini_Generated_Image_19mh6919mh6919mh_bvkg7g.png",
+    file: "posts/mysql-part-1.html",
 
-  en: {
-    title: "SQL Part 1 - 11 Dec 2025",
-    excerpt: "A data set is created using some medium ..."
+    en: {
+      title: "SQL Part 1 - 11 Dec 2025",
+      excerpt: "A data set is created using some medium ..."
+    },
+
+    si: {
+      title: "SQL 1 කොටස - 11 Dec 2025",
+      excerpt: "data set එකක් මොකක් හරි මාධ්‍යක් use කරලා ..."
+    }
   },
-
-  si: {
-    title: "SQL 1 කොටස - 11 Dec 2025",
-    excerpt: "data set එකක් මොකක් හරි මාධ්‍යක් use කරලා ..."
-  }
-},
 
   2: {
-  image: "https://res.cloudinary.com/dy07nxsrf/image/upload/v1770908633/Gemini_Generated_Image_cj3s1hcj3s1hcj3s_pdktli.png",
-  file: "posts/mysql-part-2.html",   
+    image: "https://res.cloudinary.com/dy07nxsrf/image/upload/v1770908633/Gemini_Generated_Image_cj3s1hcj3s1hcj3s_pdktli.png",
+    file: "posts/mysql-part-2.html",
 
-  en: {
-    title: "SQL Part 2 - 14 Dec 2025",
-    excerpt: "An entity is someone , a place , ..."
+    en: {
+      title: "SQL Part 2 - 14 Dec 2025",
+      excerpt: "An entity is someone , a place , ..."
+    },
+
+    si: {
+      title: "SQL 2 කොටස - 14 Dec 2025",
+      excerpt: "Entity එකක් කියන්නේ කවුරුහරි කෙනෙක් , තැනක් ,..."
+    }
   },
 
-  si: {
-    title: "SQL 2 කොටස - 14 Dec 2025",
-    excerpt: "Entity එකක් කියන්නේ කවුරුහරි කෙනෙක් , තැනක් ,..."
+  3: {
+    image: "https://res.cloudinary.com/dy07nxsrf/image/upload/v1770979627/Gemini_Generated_Image_phzuwzphzuwzphzu_nvooaa.png",
+    file: "posts/mysql-part-3.html",
+
+    en: {
+      title: "SQL Part 3 - 23 Dec 2025",
+      excerpt: "A data set is created using some medium ..."
+    },
+
+    si: {
+      title: "SQL 3 කොටස - 23 Dec 2025",
+      excerpt: "data set එකක් මොකක් හරි මාධ්‍යක් use කරලා ..."
+    }
+  },
+  4: {
+    image: "https://res.cloudinary.com/dy07nxsrf/image/upload/v1770986732/Gemini_Generated_Image_q275q8q275q8q275_qqmeh0.png",
+    file: "posts/mysql-part-4.html",
+
+    en: {
+      title: "SQL Part 4 - 24 Jan 2026",
+      excerpt: "A primary key is the data that we use to ..."
+    },
+
+    si: {
+      title: "SQL 4 කොටස - 24 Jan 2026",
+      excerpt: "data set එකක් මොකක් හරි මාධ්‍යක් use කරලා ..."
+    }
   }
-},
 };
